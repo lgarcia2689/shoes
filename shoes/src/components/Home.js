85cc6bc604dd146caa7a5d9c55452f01fdb4c752
Luis Garcia <jrluis21@gmail.com>
2021-08-22 20:28:26 -0500

    created banner

---
+++ shoes/src/components/Home.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+// import { useHistory } from 'react-router-dom'
+import './Home.css'
+
+export default function Home() {
+
+  
+  
+    return(
+        <div className = 'mainHeader'>
+            <h1>HOME</h1>
+        </div>
+
+    )
+  }
